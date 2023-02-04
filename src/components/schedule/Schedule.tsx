65bcc32660b1f861879b4c051ce5e8a9d2005574
WIN-11 <sclous2012@gmail.com>
2023-02-04 15:44:35 +0700

    feat: change Schedule

--- src/components/schedule/Schedule.tsx
+++ src/components/schedule/Schedule.tsx
@@ -1,22 +1,37 @@
-import {useState, ChangeEvent} from 'react';
+import {useState, ChangeEvent, useEffect} from 'react';
 import './schedule.css'
 
 function Schedule() {
     const [jadwal, setJadwal] = useState<string>("")
+
     const onJadwalChange = (e: ChangeEvent<HTMLInputElement>) => {
         setJadwal(e.target.value)
-        console.log(jadwal, e.target.value)
     };
 
-    const gas = () => {
-        console.log(jadwal)
+    const close = () => {
+        // untuk
     }
+
+    const saveData = () => {
+        localStorage.setItem('schedule', jadwal)
+    }
+
+    useEffect(() => {
+        document.addEventListener('DOMContentLoaded', () => {
+            let localSchedule = localStorage.getItem('schedule')
+            if (localSchedule)
+                setJadwal(localSchedule)
+            console.log(jadwal)
+        })
+    })
 
     return (
         <div>
-            <input type="text" onChange={onJadwalChange}></input>
-            <button onClick={gas}>test</button>
-            
+            <div className='bg-white h-full w-full'>
+                <input type="text" onChange={onJadwalChange}></input>
+                <button onClick={saveData}>Save</button>
+                <button onClick={() => {console.log(jadwal)}}>Just Once</button>
+            </div>
         </div>
     )
 }
