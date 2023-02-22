fb779c8f90c6ddeadcc27e2fc7d654c8aa529c47
WIN-11 <sclous2012@gmail.com>
2023-02-22 12:01:06 +0700

    fix: card

--- src/components/search/Card.tsx
+++ src/components/search/Card.tsx
@@ -6,8 +6,8 @@ interface CardProps {
 
 export default function Card({data}: CardProps) {
     return (
-        <div className="py-8 rounded-2xl bg-slate-200">
-            <div className="mb-16 border-l-2 border-red-400 px-5 py-3 flex flex-row">
+        <div className="py-8 rounded-2xl bg-slate-200 h-fit hover:bg-slate-300" >
+            <div className="mb-20 border-l-2 border-red-400 px-5 py-3 flex flex-row">
                 <div className="mx-3 h-6">
                     <a style={{fontSize: '2em'}}>{data.subjectClass!="None"? data.subjectClass:"?"}</a>
                 </div>
@@ -22,7 +22,7 @@ export default function Card({data}: CardProps) {
             </div>
 
             <div>
-                <div className="flex flex-col text-lg px-5 w-full">
+                <div className="flex flex-col text-lg px-8 w-full">
                     <div className="font-bold">
                         {data.subject}
                     </div>
@@ -37,12 +37,23 @@ export default function Card({data}: CardProps) {
 }
 
 /*
-<div className="flex justify-center mt-10 mb-10">
-            <div className="card">
-                <div>{data.subject}</div>
-                <div>{data.subjectClass}</div>
-                <div>{data.day} - {data.hour}</div>
-                <div>{data.classRoom}</div>
-            </div>
-        </div>
+<div className="flex justify-center mt-10 mb-10" key={index}>
+                                <div className="card">
+                                    <div>{item.subject}</div>
+                                    <div>{item.semester}</div>
+                                    <div>{item.sks}</div>
+                                    <div>{item.day}</div>
+                                    <div>{item.hour}</div>
+                                    {
+                                        item.lecturer?.map((item, index) => {
+                                            return (
+                                                <div key={index}>
+                                                    {item && (<div>{item.nama}</div>)}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+>>>>>>> main
 */
